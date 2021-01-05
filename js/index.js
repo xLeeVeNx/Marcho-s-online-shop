67bdbc5b3f56031c103c204ffa5623fad0d35937
xLeeVeNx <berdyev.damil1@mail.ru>
2021-01-05 19:18:31 +0600

    created an online shop Marcho

---
+++ js/index.js
@@ -0,0 +1,42 @@
+function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  }
+
+  function initializeClock(id, endtime) {
+    const clock = document.querySelector(".stock__data");
+    const daysSpan = clock.querySelector(".stock__days");
+    const hoursSpan = clock.querySelector(".stock__hours");
+    const minutesSpan = clock.querySelector(".stock__minutes");
+    const secondsSpan = clock.querySelector(".stock__seconds");
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
+      minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
+
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+  }
+
+  var deadline = new Date(Date.parse(new Date()) + 1 * 24 * 60 * 60 * 1000);
+  initializeClock("stock__data", deadline);
